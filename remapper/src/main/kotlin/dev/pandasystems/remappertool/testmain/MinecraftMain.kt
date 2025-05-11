74ef587a9d6094e2ae62ef1d65124980056fd839
The Panda Oliver <Oliver.h.froberg@gmail.com>
2025-05-11 21:13:05 +0200

    Add JSON export for mappings and update test file usage
    Introduced Gson for exporting mapping data to a JSON file with pretty printing. Modified test file paths to use smaller versions of intermediary and yarn mappings. This change ensures better debugging and testing capabilities.

--- remapper/src/main/kotlin/dev/pandasystems/remappertool/testmain/MinecraftMain.kt
+++ remapper/src/main/kotlin/dev/pandasystems/remappertool/testmain/MinecraftMain.kt
@@ -1,5 +1,7 @@
 package dev.pandasystems.remappertool.testmain
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dev.pandasystems.remappertool.applyMappings
 import dev.pandasystems.remappertool.remappers.TinyMappingsSerializer
 import dev.pandasystems.remappertool.remappers.createLayered
@@ -8,10 +10,16 @@ import kotlin.io.path.readText
 
 fun main() {
 	val path = Paths.get("remapper/testFiles/minecraft")
+	val gson = GsonBuilder().setPrettyPrinting().create()
 
-	val intermediaryMapping = TinyMappingsSerializer.deserialize(path.resolve("intermediary.tiny").readText(Charsets.UTF_8))
-	val yarnMapping = TinyMappingsSerializer.deserialize(path.resolve("yarn.tiny").readText(Charsets.UTF_8))
+//	val intermediaryMapping = TinyMappingsSerializer.deserialize(path.resolve("intermediary.tiny").readText(Charsets.UTF_8))
+//	val yarnMapping = TinyMappingsSerializer.deserialize(path.resolve("yarn.tiny").readText(Charsets.UTF_8))
+	val intermediaryMapping = TinyMappingsSerializer.deserialize(path.resolve("intermediary_small.tiny").readText(Charsets.UTF_8))
+	val yarnMapping = TinyMappingsSerializer.deserialize(path.resolve("yarn_small.tiny").readText(Charsets.UTF_8))
 	val mappings = intermediaryMapping.createLayered(yarnMapping)
+	
+	val classes = mappings.content
+	path.resolve("log/mapping_data.json").toFile().also { it.parentFile.mkdirs() }.writeText(gson.toJson(classes))
 
 	mappings.applyMappings(
 		"official",
